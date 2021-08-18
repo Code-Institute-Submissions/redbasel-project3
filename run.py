538f5ee27d128b7260f864de96534b9bdb290f5e
redbasel <jansafar014@gmail.com>
2021-08-18 17:23:25 +0000

    Updated comments, moved inside functions

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # we need gspread in order to access the sheet.
 # pprint is to better present the tables to the users
-from pprint import pprint as pp
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
 # the scope says which apis can be utilized
@@ -15,15 +15,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('p3clients')
 
-"""
-this functon fetches and manipulates the values,
-from the selected client to be used with the tools i.e. our functions
-"""
 
 
 def to_be_utilized():
+    """
+    this function fetches and manipulates the values,
+    from the selected client to be used with the tools i.e. our functions
+    """
     try:
-        select_worksheet = input('Name of the client you wish to access:\n')
+        select_worksheet = input('Name of the client you wish to access:\n').lower()
         terminal_chosen_worksheet = SHEET.worksheet(select_worksheet)
         chosen_ws_all_values = terminal_chosen_worksheet.get_all_values()
         header_row = chosen_ws_all_values[0]
@@ -42,29 +42,31 @@ def to_be_utilized():
         print("Restarting the application")
         main()
 
-"""
-for loop that calculates the percentage change of client data over time,
-comparing the first measurement with the last.
-"""
+
 
 
 def health_measurements():
+    """
+    for loop that calculates the percentage change of client data over time,
+    comparing the first measurement with the last.
+    """
     select_worksheet, terminal_chosen_worksheet, chosen_ws_all_values, header_row, start_row, last_row, start_weight_test, weight_final_value, height_value, start_bmi, final_bmi, percentage_change_test_bmi = to_be_utilized()
     for x in range(4):
         percentage_change_test = int(((int(last_row[x])) - (int(start_row[x])))/(int(start_row[x])) * 100)
         header_row_x = header_row[x]
         print(f"the change in {header_row_x} is {percentage_change_test}%")
 
-"""
-this function calcualtes the bmi of the client.
-Firstly their initial bmi value.
-Secondly, their final bmi vlaue.
-Lastly, it outputs to the client in which bmi range they were,
-and which they are presently in.
-"""
+
 
 
 def bmi_check():
+    """
+    this function calcualtes the bmi of the client.
+    Firstly their initial bmi value.
+    Secondly, their final bmi vlaue.
+    Lastly, it outputs to the client in which bmi range they were,
+    and which they are presently in.
+    """
     select_worksheet, terminal_chosen_worksheet, chosen_ws_all_values, header_row, start_row, last_row, start_weight_test, weight_final_value, height_value, start_bmi, final_bmi, percentage_change_test_bmi = to_be_utilized()
     if start_bmi < 18.5:
         bmi_interval_past = "underweight"
@@ -119,41 +121,48 @@ def bmi_check():
     print(f"\n{select_worksheet} was in the {bmi_interval_past} range,")
     print(f"now {select_worksheet} is in the {bmi_interval_present} range. \n")
 
-"""
-This function prints the table of the client,
-of which the first row is filled with the titles
-"""
+
 
 
 def all_data_client():
+    """
+    This function prints the table of the client,
+    of which the first row is filled with the titles
+    """
     select_worksheet, terminal_chosen_worksheet, chosen_ws_all_values, header_row, start_row, last_row, start_weight_test, weight_final_value, height_value, start_bmi, final_bmi, percentage_change_test_bmi = to_be_utilized()
-    pp(chosen_ws_all_values)
+    pprint(chosen_ws_all_values)
 
-"""
-This is the menu that the user will interact with initially,
-then return to again to continue using the other functions available.
-"""
 
 
 def tool_menu():
+    """
+    This is the menu that the user will interact with initially,
+    then return to again to continue using the other functions available.
+    """
     print("\nWelcome to the tool menu, these tools are at your disposal")
     print("1: Analyze the clients percentage change in body measurements")
     print("2: Analyze the clients past and present BMI data")
     print("3: Show all the data available")
-    print('available clients:')
     worksheet_list = SHEET.worksheets()
     ws_names = ','.join(str(v) for v in worksheet_list)
     try:
-        for sheet in worksheet_list:
-            print(sheet.title)
         selection = int(input("Please enter your selection, between 1-3:\n"))
         if selection == 1:
+            print('Available clients:')
+            for sheet in worksheet_list:
+                print(sheet.title)
             health_measurements()
             run_again()
         elif selection == 2:
+            print('Available clients:')
+            for sheet in worksheet_list:
+                print(sheet.title)
             bmi_check()
             run_again()
         elif selection == 3:
+            print('Available clients:')
+            for sheet in worksheet_list:
+                print(sheet.title)
             all_data_client()
             run_again()
         else:
@@ -162,29 +171,31 @@ def tool_menu():
     except ValueError:
         print("Please, enter a valid selection, make sure its an integer")
         run_again()
-"""
-This function main task is once tool_menu finishes,
-ask if the user is finished or wish to utilize the menu once more
- """
+
 
 
 def run_again():
-    answer = input('To continue, type "yes" otherwise "no" \n')
+    """
+    This function main task is once tool_menu finishes,
+    ask if the user is finished or wish to utilize the menu once more
+    """
+    answer = input('To continue, type "yes" otherwise "no". \n').lower()
     if answer == 'yes':
         tool_menu()
     elif answer == 'no':
-        print('Thank you for using the tool, hopefully it could assist you')
+        print('Thank you for using the tool, hopefully it could assist you.')
         exit()
     else:
         print(" Don't use uppercase or integers.")
         run_again()
-"""
-this is the function that starts the application,
-doing it this way makes it possible to add elements in the future
-"""
+
 
 
 def main():
+    """
+    this is the function that starts the application,
+    doing it this way makes it possible to add elements in the future
+    """
     tool_menu()
 
 main()
